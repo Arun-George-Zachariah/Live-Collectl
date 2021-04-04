fe5df14711c36df838b6c4a46b233fa81053a5f0
Arun George Zachariah <azachariah@mail.missouri.edu>
2021-04-04 17:10:55 -0500

    Changes done to read the CPU plot files - Bug fixes.

--- src/edu/missouri/constants/Constants.java
+++ src/edu/missouri/constants/Constants.java
@@ -3,12 +3,12 @@ package edu.missouri.constants;
 public class Constants {
 
     public static final String HASH = "#";
-    public static final String DIR_SEPERATOR = "/";
+    public static final String DIR_SEPARATOR = "/";
 
     public static final String DATA_FILE = "";
 
     public static final String DATA_DIR = "/mydata/EVA_Space/Live-Collectl/Data"; // To-Do: Should be read from a conf file.
-    public static final String CPU_FILE = DATA_DIR + DIR_SEPERATOR + "vm0-20210322.cpu"; // To-Do: Should be read from a conf file.
+    public static final String CPU_FILE = DATA_DIR + DIR_SEPARATOR + "vm0-20210322.cpu"; // To-Do: Should be read from a conf file.
 
 
     
